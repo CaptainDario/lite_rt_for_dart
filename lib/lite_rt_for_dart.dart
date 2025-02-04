f90017f3b150712176ad23b8940346c49194a991
CaptainDario @ MBP M1 <daapplab@gmail.com>
2025-02-04 21:55:55 +0900

    load library from path

--- lib/lite_rt_for_dart.dart
+++ lib/lite_rt_for_dart.dart
@@ -5,6 +5,8 @@
 /// TensorFlow Lite for Flutter
 library lite_rt_for_dart;
 
+import 'dart:ffi';
+
 import 'package:ffi/ffi.dart';
 import 'package:lite_rt_for_dart/src/bindings/bindings.dart';
 import 'package:lite_rt_for_dart/src/bindings/tensorflow_lite_bindings_generated.dart';
@@ -25,16 +27,35 @@ export 'src/util/list_shape_extension.dart';
 /// tflite version information.
 String get tfLiteVersion => tfliteBinding.TfLiteVersion().cast<Utf8>().toDartString();
 
-/// Call this **before** using any methods of this package
+/// Call this or `` **before** using any methods of this package
+/// 
+/// Pass a path to a tf lite dynmic library
 void initLiteRT(String libraryPath, {
     String? gpuDelegatelibraryPath
   }){
 
-  tfliteBinding = TensorFlowLiteBindings(loadLiteRTLib(libraryPath));
+  tfliteBinding = TensorFlowLiteBindings(DynamicLibrary.open(libraryPath));
 
   // add gpu delegate if set
   if(gpuDelegatelibraryPath != null){
-    tfliteBindingGpu = TensorFlowLiteBindings(loadLiteRTLib(gpuDelegatelibraryPath));
+    tfliteBindingGpu = TensorFlowLiteBindings(
+      DynamicLibrary.open(gpuDelegatelibraryPath));
+  }
+
+}
+
+/// Call this or `initLiteRT` **before** using any methods of this package
+/// 
+/// Pass a DynamicLibrary that holds the TFLite runtime symbols
+void initLiteRTFromLib(DynamicLibrary library, {
+    DynamicLibrary? gpuDelegatelibrary
+  }){
+
+  tfliteBinding = TensorFlowLiteBindings(library);
+
+  // add gpu delegate if set
+  if(gpuDelegatelibrary != null){
+    tfliteBindingGpu = TensorFlowLiteBindings(gpuDelegatelibrary);
   }
 
 }
