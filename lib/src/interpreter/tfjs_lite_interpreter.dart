6409e47dc17dd638d2c55b620b07378a3232470f
CaptainDario @ MBP M1 <daapplab@gmail.com>
2025-02-10 11:41:24 +0900

    tfjs lite based interpreter for web

---
+++ lib/src/interpreter/tfjs_lite_interpreter.dart
@@ -0,0 +1,23 @@
+import 'dart:typed_data';
+
+
+
+class TfJsLiteInterpreter {
+
+  
+
+  TfJsLiteInterpreter._();
+
+  factory TfJsLiteInterpreter.fromUrl(String url){
+
+    return TfJsLiteInterpreter._();
+
+  }
+
+  factory TfJsLiteInterpreter.fromBuffer(Uint8List){
+
+    return TfJsLiteInterpreter._();
+
+  }
+
+}
